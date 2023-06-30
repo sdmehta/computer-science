44b59843afa0ac05e0e9dc264e38d9b398551cdf
Swapnil Mehta <48033671+swapnildmehta@users.noreply.github.com>
2023-06-30 23:55:41 +0530

    test scanner

--- src/com/demo/COSC2P03/assignment3/src/testheap/TestScanner.java
+++ src/com/demo/COSC2P03/assignment3/src/testheap/TestScanner.java
@@ -1,40 +1,17 @@
 package com.demo.COSC2P03.assignment3.src.testheap;
 
-import com.demo.COSC2P03.assignment3.src.storage.IndexedHeap;
-import com.demo.COSC2P03.assignment3.src.storage.IndexedHeapTree;
 
 public class TestScanner {
+    static class Pair {
+        int x = 5;
+        int y = 10;
+    }
+
+
     public static void main(String[] args) {
-        IndexedHeap<String> minHeap = new IndexedHeapTree<>(String.CASE_INSENSITIVE_ORDER);
+        Integer y = 10;
 
-        minHeap.insert("asd");
-        minHeap.insert("kml");
-        minHeap.insert("zjn");
-        minHeap.insert("edf");
-        minHeap.insert("inh");
-        minHeap.insert("xdf");
-        System.out.println(minHeap.getAtIndex(1));
-        System.out.println(minHeap.getAtIndex(2));
-        System.out.println(minHeap.getAtIndex(3));
-        System.out.println(minHeap.getAtIndex(4));
-        System.out.println(minHeap.getAtIndex(5));
-        System.out.println(minHeap.getAtIndex(6));
-
-
-        IndexedHeap<String> maxHeap = new IndexedHeapTree<>(String.CASE_INSENSITIVE_ORDER.reversed());
-
-        maxHeap.insert("asd");
-        maxHeap.insert("kml");
-        maxHeap.insert("zjn");
-        maxHeap.insert("edf");
-        maxHeap.insert("inh");
-        maxHeap.insert("xdf");
-
-        System.out.println(maxHeap.getAtIndex(1));
-        System.out.println(maxHeap.getAtIndex(2));
-        System.out.println(maxHeap.getAtIndex(3));
-        System.out.println(maxHeap.getAtIndex(4));
-        System.out.println(maxHeap.getAtIndex(5));
-
+        Pair p = new Pair();
+        System.out.println(y);
     }
 }
